649233d34b446375d6f0de7c5f7b3f64049c4143
Adam <Adamwall97@gmail.com>
2022-04-04 15:39:05 -0500

    Setting Up React Page and Outline

---
+++ personalpage/src/AboutMe.js
@@ -0,0 +1,28 @@
+function AboutMe() {
+    
+  return (
+    <div className="App">
+      <header>
+        About Page
+        My Skills
+        Projects
+        Contact Me
+      </header>
+
+<Body>
+
+<Project>
+  
+</Project>
+
+</Body>
+
+<Footer>
+
+</Footer>
+
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ personalpage/src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <NavBar>
+        Buttons with Links to new pages
+        - About Me
+        - Contact Me
+        - Main Page
+        - Link to Linked In
+
+      </NavBar>
+
+      <Header>
+        Photo of Myself
+
+        Adam Wall
+
+        Info About Adam
+
+        Welcome to my Website!
+
+      </Header>
+
+<Projects>
+
+Projects I've worked on 
+
+  <Project>
+  ScreenSpeak
+  </Project>
+
+  <Project>
+  API Focused
+  </Project>
+
+  <Project>
+  
+  </Project>
+
+
+</Projects>
+
+<Footer>
+
+
+</Footer>
+
+    </div>
+  );
+}
+
+export default App;
