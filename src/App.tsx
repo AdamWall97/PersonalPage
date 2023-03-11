3cec8be1032f9d8767ee8aebff1cfa209f031048
Adam <adamwall97@gmail.com>
2023-03-10 20:47:49 -0600

    Add Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'reactstrap';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> <Button color="danger">Test Bootstrap</Button>and save to reload.
         </p>
