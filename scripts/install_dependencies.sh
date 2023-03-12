99ea4f32fd29008210f472e7d5d446c05ba18d59
Adam <adamwall97@gmail.com>
2023-03-12 13:10:34 -0500

    Add BuildSpec to copy build files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,11 +2,11 @@
 
 #stdout logs of this process executing can be found in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/logs/scripts.log
 
-
 # here we update the server and install node and npm
 echo installing dependencies
 sudo apt-get update
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install nginx -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get -y install npm
 
