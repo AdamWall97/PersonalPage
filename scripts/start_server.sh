607301f10de295f043b6303ed4594c3d3b80384f
Adam <adamwall97@gmail.com>
2023-03-11 18:32:12 -0600

    Add in Deploy scripts

---
+++ scripts/start_server.sh
@@ -0,0 +1,10 @@
+echo starting server
+
+# here we just use npm to run the build
+cd /var/www/
+echo building application...
+sudo npm run build
+
+# start the application with pm2
+echo starting application...
+sudo pm2 start npm -- run production
---
+++ scripts/validate_server.sh
@@ -0,0 +1,4 @@
+
+# for now we are just checking that we get back a page from the server
+echo validating server
+#curl -m 5 http://localhost:8080
